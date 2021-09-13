db5694eaf1b84d42cc5dcffd8048acf78b240be4
rekhaselvan01 <rekhaselvan01@gmail.com>
2021-09-13 10:54:12 -0400

    second version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # my-password-generator
+
+The first version of password generator was only able to achieve few things
+1. user input was collected using prompt.
+2. the inputs were validated.
+3. the generate password method was not implemented in this version.
+********************************************
+The second version of password generator was still not able to generate correct password
+
+1. I was able to make the character sets as array, uppercase,lowercase, splchars, number.
+2. I used math.random to generate random numbers but not able to convert random numberrs to character codes.. tried using charcode, string.fromcharcode function.. still not able to achieve..
+just errors.
+3. At this point when generate password button is clicked, the application is only able to display random numbers.
+
+Moving on to third version:
+Areas to be fixed
+1. validation has loop hole that need to be fixed, if user enters wrong value for third time.. the prompt doesn't pop up... 
+2. I am not satisfied with flow of code.. it would be better if i get values in one function and validate in other.. right now its messy.
+3.Hopefully i will be able to fix the algorithm and display the correct password.
+screenshot for this version:
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-/*// Assignment Code
+
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generateWeakestPassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,75 +14,129 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-*/
 
+
+
+
+// Constants
+const constMinPasswordLength = 8;
+const constMaxPasswordLength = 128;
+
+//Execution starts here
 var genPassword= confirm("Do you want to generate a password?:");
 alert(genPassword);
 console.log(genPassword);
+
+//If user confirms that password needs to be generated
 if(genPassword){
-  minLengthValidation();    
-  splCharValidation();
-  /*generatePassword();*/
+    // Ask user to choose length of password
+    var passwordLength = getPasswordLength();    
+
+    // Validate the password length
+
+    // Choose a lenght of password to generate - we are going to keep min lenght for now
+
+
+    // Ask all chars that needs to be included in the password
+    getCharsToUse();
+
+    // Validate for char to use
+
+    // Generate the password based on above parameters 
+   // generateWeakestPassword();
 }
 
-function minLengthValidation(){
+function getPasswordLength(){
+    var minLengthPasswd= prompt("Choose minimum length of password (should be greater than " + constMinPasswordLength + ")", '');
+     if(minLengthPasswd < constMinPasswordLength){     
+        var minPasswordLengthUserInput = confirm("Password should have atleast eight characters");
+        //TODO : Need to decide when to terminate and restart the program 
+        if(minPasswordLengthUserInput){ 
+            var CnfrmMinLength = prompt("Choose minimum length of password again (should be greater than " + constMinPasswordLength + ")", '');
+        }
+    } else if(minLengthPasswd >= constMinPasswordLength){
+        var maxLenPasswd= prompt("Choose maximum length of password", '');
+        if(maxLenPasswd > constMaxPasswordLength){
+            var cnfrmMaxLength= confirm("Password should not have more than "+ constMaxPasswordLength + " character");
+            //TODO : Need to decide when to terminate and restart the program 
+            if(cnfrmMaxLength){
+                var secCnfrmMinLength = prompt("Choose maximum length of password", '');
+            }
+        }
+    }
+    return [minLengthPasswd, maxLenPasswd];
+}
+
+function getCharsToUse() {
+    window.useLowerCase = confirm("Do you want to include lowercase?");
+    window.useUpperCase = confirm("Do you want to include uppercase?");
+    window.useNumbers = confirm("Do you want to include numbers?");
+    window.useSpecialChars = confirm("Do you want to include special characters?");
+    console.log(useLowerCase);
+    console.log(useUpperCase);
+    console.log(useNumbers);
+    console.log(useSpecialChars);
+    if((useLowerCase || useUpperCase || useSpecialChars|| useNumbers) === true){
+        console.log("User selected at least one char to be used");
+    }
+    else{
+        console.log("User did not select one option");
+        alert("Please select at least one char type");
+        //TODO: Need to decide what to do next - restart from beginning or continue from here
+    }
+}
+
+
+   function generateWeakestPassword(){
   
-    var minLengthPasswd= prompt("choose minimum length of password", '');
-     if(minLengthPasswd<8){
-         
-        var confrmMinLength = confirm("Password should have atleast eight characters");
-        if(confrmMinLength){ 
-            var CnfrmMinLength = prompt("choose minimum length of password", '');
+    var incldCharCount=0;
+    if(useLowerCase){
+incldCharCount ++;
     }
-}
-else if(minLengthPasswd >=8){
-    maxLenPasswd= prompt("Choose maximum length of password", '');
-    if(maxLenPasswd > 128){
-        var cnfrmMaxLength= confirm("Password should not have more than 128 character");
-    if(cnfrmMaxLength){
-        var secCnfrmMinLength = prompt("choose maximum length of password", '');
+    if(useNumbers){
+        incldCharCount++;
     }
+    if(useUpperCase){
+        incldCharCount++;
     }
-}
-}
-
-function splCharValidation() {
+    if(useSpecialChars){
+        incldCharCount++;
+    }
+console.log(incldCharCount);
+var defaultArryCount= parseInt(passwordLength[0])- parseInt(incldCharCount);
+console.log(defaultArryCount);
+intrmediatePassword= [];
+for(var i=0; i<= defaultArryCount; i++){
+    const defaultArray = [Math.floor(Math.random() * defaultArryCount)];
     
-    var lwrCaseValidation= confirm("do you want to include lowercase?");
-    var uprCaseValidation= confirm("do you want to include uppercase?");
-    var numValidation= confirm("do you want to include numbers?");
-    var splCharValidation= confirm("do you want to include special characters?");
-   console.log(lwrCaseValidation);
-   console.log(uprCaseValidation);
-   console.log(numValidation);
-   console.log(splCharValidation);
+    console.log(defaultArray)
+    //intrmediatePassword.push(String.fromCharCode(defaultArray));
+    intrmediatePassword.push(defaultArray);
+    console.log(intrmediatePassword);
 
-   if((lwrCaseValidation || uprCaseValidation || splCharValidation|| numValidation)===true){
-       console.log("atleast one is selected");
-   }
-   else{
-       console.log("you have to select atleast one");
-       alert("you need to select atleast one special character");
-      
-   }
-  
-   }
+}
+return intrmediatePassword
+//console.log(intrmediatePassword.join(''));
 
-   /*function generatePassword(lenPassword, lwrCaseValidation,uprCaseValidation,numValidation,splCharValidation) {
-       lenPassword=(maxLenPasswd-minLengthPasswd);
-       console.log(lenPassword);
-       String.fromCharCode(65);
-      function arrayCode(startValue,endValue){
-          
-          for(let i=startValue; i<=endValue; i++){
-
-          }
-      }
-           
-       });
+   
+}
+//creating array to make characterarrays for each character type
+function insertArrayValues(startvalue,endvalue){
+    const intlarray= [];
+    for(let i=startvalue; i<= endvalue; i++){
+        intlarray.push(i);
+    }
+return intlarray
+console.log(intlarray)
+}
 
 
-   }*/
+  const uprCase = insertArrayValues(65,90);
+  console.log(uprCase);
+  const lwrCase= insertArrayValues(97,122);
+  console.log(lwrCase);
+  const numCase= insertArrayValues(48,57);
+  console.log(numCase);
+  const splCase= insertArrayValues(33,47).concat(insertArrayValues(58,64)).concat(insertArrayValues(91,96)).concat(insertArrayValues(123,126));
+  console.log(splCase);
 
-
-    
