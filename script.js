5293de54b67c6444fd0bde9206e52ba979f0bf00
rekhaselvan01 <rekhaselvan01@gmail.com>
2021-09-14 16:43:43 -0400

    final version

--- script.js
+++ script.js
@@ -1,84 +1,114 @@
-//grab elements using querySelector
-//Grab button element by id and assign variable
-var btn = document.querySelector("#generate");
-//Grab textarea by id and assign variable
-var textarea = document.querySelector("#password");
 
-//Assign values to variables
-//Assign return value of function generatePassword to variable password
-var password =generatePassword();
-//assign value of the password to textarea
-textarea.value = password;
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-//Add eventlisteners to button item
-//add eventListener to generatePassword button
-btn.addEventListener("click",generatePassword());
+// Write password to the #password input
+function writePassword() {
+  var password = generateWeakestPassword();
+  console.log(password);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+  //Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
-//call functions 
-getuserinputs();
-validateMinLength();
-validateMaxLength();
 
-//Get user inputs
-//create variables to get input
-var generatePassword;
-var minLengthPassword;
-var maxLengthPassword;
-var useLowerCase;
-var useUpperCase;
-var useNumbers;
-var useSplCharacters;
 
-//write a function to getuserinputs
-//get user inputs using prompt and assign values to variables
-function getuserinputs()
-{
-generatePassword = prompt("Do you want to generate Password?", '');
-console.log(generatePassword);
-minLengthPassword = prompt("Enter minimum length of the password:",'');
-console.log(minLengthPassword);
-maxLengthPassword = prompt("Enter Maximum length of the password:" ,'');
-console.log(maxLengthPassword);
-useLowerCase = prompt("Do you want to use Lowercase?",'');
-console.log(useLowerCase);
-useUpperCase = prompt("Do you want to use UpperCase?:", '');
-console.log(useUpperCase);
-useNumbers = prompt("Do you want to use Numbers?:", '');
-console.log(useNumbers);
-useSplCharacters = prompt("Do you want to use SpecialCharacters?:", '');
-console.log(useSplCharacters);
+// Constants
+const constMinPasswordLength = 8;
+const constMaxPasswordLength = 128;
+const uprCaseCharSet ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lwrCaseCharSet= 'abcdefghijklmnopqrstuvwxyz';
+const numCharSet= '0123456789';
+const splCharSet= '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+
+//Execution starts here
+var genPassword= confirm("Do you want to generate a password?:");
+alert(genPassword);
+console.log(genPassword);
+
+//If user confirms that password needs to be generated
+if(genPassword){
+    // Ask user to choose length of password
+    var passwordLength = getPasswordLength();    
+
+
+
+    // Ask all chars that needs to be included in the password
+    getCharsToUse();
+    generateWeakestPassword();
+
 }
 
-//write a function to validate Minimum length of password
-function validateMinLength(){
-    if(generatePassword){
-
-        if(minLengthPassword<8){
-            var confrmMinLength= confirm("The minimum length of Password should be eight characters", '');
-                               }    
-                        
-              if(confrmMinLength){
-                  getuserinputs();
-                                 }          
-                        }
-                            }
-
-//write a function to validate Maximum length of password
-function validateMaxLength(){
-    if(generatePassword){
-
-        if(maxLengthPassword>128){
-            var confrmMaxLength= confirm("The maximum length of Password should be 128 characters", '');
-                               }    
-                        
-              if(confrmMaxLength){
-                  getuserinputs();
-                                 }          
-                        }
-                            }
-
-                            
-function generatePassword(){
-    console.log("hello");
+function getPasswordLength(){
+    var minLengthPasswd= prompt("Choose minimum length of password (should be greater than " + constMinPasswordLength + ")", '');
+     if(minLengthPasswd < constMinPasswordLength){     
+        var minPasswordLengthUserInput = confirm("Password should have atleast eight characters");
+        //TODO : Need to decide when to terminate and restart the program 
+        if(minPasswordLengthUserInput){ 
+            var CnfrmMinLength = prompt("Choose minimum length of password again (should be greater than " + constMinPasswordLength + ")", '');
+        }
+    } else if(minLengthPasswd >= constMinPasswordLength){
+        var maxLenPasswd= prompt("Choose maximum length of password", '');
+        if(maxLenPasswd > constMaxPasswordLength){
+            var cnfrmMaxLength= confirm("Password should not have more than "+ constMaxPasswordLength + " character");
+            //TODO : Need to decide when to terminate and restart the program 
+            if(cnfrmMaxLength){
+                var secCnfrmMinLength = prompt("Choose maximum length of password", '');
+            }
+        }
+    }
+    return [minLengthPasswd, maxLenPasswd];
 }
+
+function getCharsToUse() {
+    window.useLowerCase = confirm("Do you want to include lowercase?");
+    window.useUpperCase = confirm("Do you want to include uppercase?");
+    window.useNumbers = confirm("Do you want to include numbers?");
+    window.useSpecialChars = confirm("Do you want to include special characters?");
+    console.log(useLowerCase);
+    console.log(useUpperCase);
+    console.log(useNumbers);
+    console.log(useSpecialChars);
+    if((useLowerCase || useUpperCase || useSpecialChars|| useNumbers) === true){
+        console.log("User selected at least one char to be used");
+    }
+    else{
+        console.log("User did not select one option");
+        alert("Please select at least one char type");
+        //TODO: Need to decide what to do next - restart from beginning or continue from here
+    }
+}
+
+
+   function generateWeakestPassword(){
+
+  let defaultCharSet = lwrCaseCharSet;
+  if(useUpperCase){
+      defaultCharSet = defaultCharSet.concat(useUpperCase);
+  }
+  if(useNumbers){
+    defaultCharSet = defaultCharSet.concat(useNumbers);
+}
+if(useSpecialChars){
+    defaultCharSet = defaultCharSet.concat(useSpecialChars);
+}
+intermediatePassword= [];
+for(var i=0; i<= passwordLength[1]; i++){
+    var defaultArray = [Math.floor(Math.random() * passwordLength[1])];
+
+    console.log(defaultArray)
+    intermediatePassword.push(defaultArray);
+   defaultCharSet = intermediatePassword.concat(defaultCharSet);
+  //defaultCharSet= defaultArray.concat(defaultCharSet);
+   console.log(defaultCharSet);
+
+}
+//return intermediatePassword;
+//console.log(intermediatePassword);
+return defaultCharSet;
+console.log(defaultCharSet);
+}
+
